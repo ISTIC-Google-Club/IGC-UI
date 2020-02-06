a0282309e5518eec736efbea53c723b067b4e776
hakimmaaouia <hakimmaaouia@gmail.com>
2020-02-06 22:37:20 +0100

    gridChromebook

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-
-import {Grid,Col} from './components/grid/grid'
+import { Grid, Col } from "./components/grid/grid";
 
 const App = () => {
   return (
     <div className="App">
-       <Grid colgap={12} rowgap={12}>  
-       <Col>2</Col>
+      <Grid gap={15} temps={3}>
+        <Col span={2}>2</Col>
         <Col>2</Col>
         <Col>3</Col>
         <Col>4</Col>
-        
-       
-       
-      </Grid> 
+        <Col>2</Col>
+        <Col>2</Col>
+        <Col>3</Col>
+        <Col>4</Col>
+      </Grid>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/grid/grid.tsx
+++ src/components/grid/grid.tsx
@@ -1,50 +1,60 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
+interface types_grid {
+  colgap?: number;
+  rowgap?: number;
+  gap?: number;
+  temps?: number;
+  auto?: boolean;
+  children?: any;
+}
 
-
-
-const Grid=(props:any)=>{
-    console.log(props.children);
-    const Row= styled.div`
-    display: grid;
-    grid-template-columns: auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto;
-    grid-gap:${props.gap}px;
-    grid-row-gap:${props.rowgap}px;
-    grid-column-gap: ${props.colgap}px;
-    @media (max-width: 900px) {
-        display: block;
+const Grid = ({ colgap, rowgap, gap, temps, auto, children }: types_grid) => {
+  let temp = "";
+  var i: number;
+  if (auto) {
+    for (i = 0; i < children.length; i++) {
+      temp = temp + " auto";
+    }
+  } else if (temps) {
+    for (i = 0; i < temps; i++) {
+      temp = temp + " auto";
+    }
+  } else {
+    temp =
+      "auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto";
   }
-    
-  `
-    return(
-        
-        <div>
-            <Row>
-      {props.children}
-            </Row>
-            </div>
-    )
+
+  const Row = styled.div`
+    display: grid;
+    grid-template-columns: ${temp};
+    grid-gap: ${gap}px;
+    grid-row-gap: ${rowgap}px;
+    grid-column-gap: ${colgap}px;
+    @media (max-width: 900px) {
+      display: block;
+    }
+  `;
+  return (
+    <div>
+      <Row>{children}</Row>
+    </div>
+  );
+};
+
+interface types_col {
+  span?: number;
+  children?: any;
 }
 
+const Col = ({ span, children }: types_col) => {
+  const Cols = styled.div`
+    background: red;
+    grid-column: span ${span};
+  `;
 
+  return <Cols>{children}</Cols>;
+};
 
-const Col=(props:any)=>{
-   
-    
-    const Cols= styled.div`
-   background:red;
-   grid-column: span ${props.span};
-  `
-  
-    return(
-         <Cols>{props.children}</Cols> 
-    )
-}
-
-
-
-export {
-    Grid,
-    Col,
- };
+export { Grid, Col };
